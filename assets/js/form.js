76dd3adbc12f15f72450d045e26657d09ebe5625
l.maines <lmmaines28@gmail.com>
2024-05-12 21:35:04 -0400

    moved the javascript for the dark mode to the logic.js file as it applies to both webpages. began the html and css for page 2.

--- assets/js/form.js
+++
@@ -1,8 +0,0 @@
-const darkModeBtn = document.getElementById('dark-mode-btn');
-const body = document.body; 
-
-darkModeBtn.addEventListener('click', function() {
-    body.classList.toggle('dark-mode');
-    const isDarkMode = body.classList.contains('dark-mode');
-    darkModeBtn.textContent = isDarkMode ? '☀️' : '🌙'; 
-});
